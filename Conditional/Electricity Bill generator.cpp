5020aa8e2fad58fb925fc1f862fce25ca60f15d1
Niladri-Pan <Niladripan12@gmail.com>
2026-05-29 09:37:32 +0530

    Update Electricity Bill generator.cpp

--- Conditional/Electricity Bill generator.cpp
+++ Conditional/Electricity Bill generator.cpp
@@ -4,20 +4,26 @@ using namespace std;
 int main(){
 
     
-    int U;
+    float U, Bill = 0;      //Unit Consumed & Bill = 0 bcz in c++ if not mentioned it contains garbage value which will give error sometimes in calc....
     cout << "Enter Units Consumed = ";
     cin >> U;
-    float Bill;
 
 
+    int Service_charge;
+    Service_charge = 50;
 
-
-    if(U >= 100){
-        cout <<"Bill = " , Bill = U*40;
-    }else if(U >= 200){
-        cout << "Bill", Bill = U*50;
-    }else if(U > 300){
-        cout << "Bill = " , Bill = U*60;
+    if(U <= 100){
+        Bill = U*1;
+    }else if(U <=200){
+        Bill = (100*1)+((U - 100)*2);     // (U - 100/200) IS the remaining units
+    }else{
+        Bill = (100*1) + (100*2) + ((U - 200)*2.5);
     }
+
+    Bill = Bill + Service_charge;
+    cout << "Electricity Bill Rs. = " << Bill;
+
+
+
     return 0;
 }
